a6dc4a84989966917c224c52b1b993d91076be0a
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-28 18:11:58 +0800

    Introduced list comprehension

--- main.py
+++ main.py
@@ -25,11 +25,9 @@ for (index, row) in student_data_frame.iterrows():
 # 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 data = pd.read_csv('nato_phonetic_alphabet.csv')
-nato_dict = {}
-for (index, row) in data.iterrows():
-    nato_dict[row.letter] = row.code
+nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# 2. Create a list of the phonetic code words from a word that the user inputs.
 
 user_input = input("Enter a word: ").upper()
 nato = []
