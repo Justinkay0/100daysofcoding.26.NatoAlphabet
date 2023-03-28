1776bbf97eb0bdb69bbd7fcd937dd4dda2b43d07
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-28 18:16:36 +0800

    Introduced list and dictionary comprehension

--- main.py
+++ main.py
@@ -30,8 +30,6 @@ nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 # 2. Create a list of the phonetic code words from a word that the user inputs.
 
 user_input = input("Enter a word: ").upper()
-nato = []
-for letter in user_input:
-    nato.append(nato_dict[letter])
+nato = [nato_dict[i] for i in user_input]
 
 print(nato)
